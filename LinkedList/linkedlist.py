0ff286bef31cbc2bac071dc1e3be96494251c35e
dkgohel <darshan.gohel619@gmail.com>
2020-09-29 18:25:04 +0300

    Singly Linked List Python

---
+++ LinkedList/linkedlist.py
@@ -0,0 +1,126 @@
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
+            itr = itr.next
+        print(llstr)
+
+    def get_length(self):
+        count = 0
+        itr=self.head
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+
+    def insert_at_begining(self,data):
+        node = Node(data,self.head)
+        self.head=node
+    
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count+=1
+
+    def insert_values(self, data_list):
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def insert_after_value(self,data_after,data_after_insert):
+        itr=self.head
+        flag=False
+        while itr:
+            if itr.data == data_after:
+                flag=True
+                itr.next = Node(data_after_insert,itr.next)
+                break
+            itr=itr.next
+        if flag==False:
+            print("Value does not exist in LinkedList")
+    
+    def remove_first_occur(self,data):
+        itr=self.head
+        count=0
+        flag=False
+        while itr:
+            if itr.data==data:
+                flag=True
+                self.remove_at(count)
+                break
+            itr=itr.next
+            count+=1
+        if flag==False:
+            print("Value does not exist in LinkedList")
+
+
+l = LinkedList()
+l.insert_at_begining(5)
+l.insert_at_begining(10)
+l.insert_at_end(9)
+l.insert_values([4,6,13,15])
+l.remove_at(4)
+l.insert_at(5,22)
+l.print()
+l.insert_after_value(4,69)
+l.print()
+l.insert_at_end(69)
+l.print()
+l.remove_first_occur(69)
+l.print()
